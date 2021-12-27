8829e7faea18c671c64187061d64b36393e08de7
Mohamed Alaa <mohamedalaashafik98@gmail.com>
2021-12-27 22:40:43 +0200

    Balance Parentheses program V1.0

---
+++ StackProgram.c
@@ -0,0 +1,99 @@
+/*******************************************************************/
+/******************** Date: 24/12/2021 *****************************/
+/******************** Project: Stack Data Structure ****************/
+/******************** Author: Mohamed Alaa *************************/
+/******************** Version: 1.0 *********************************/
+/*******************************************************************/
+
+#include<stdio.h>
+#include<stdint.h>
+
+#define size 10 //size of the stack
+
+/*Declaration of some functions*/
+void Push(uint8_t u8_data);
+uint8_t Pull(void);
+void PrintStack(void);
+uint8_t* balancedparentheses(uint8_t* u8arr_expression);
+/*global stack array and its top*/
+uint8_t gu8arr_Stackarr[size];
+int8_t gu8_Top=-1;
+
+void main(void)
+{
+	uint8_t u8arr_expression[20],*u8_status=NULL;
+	printf("Please enter your script: ");
+	gets(u8arr_expression);//ask user to enter the string
+	u8_status=balancedparentheses(u8arr_expression);//call balancedparentheses function and save the address of its return in status pointer
+	printf("%s",u8_status);//print status
+}
+
+uint8_t* balancedparentheses(uint8_t* u8arr_expression)
+{
+	/*Declare some local variables "iteration variables, count parentheses"*/
+	uint8_t u8_i,u8_j,u8_count1=0,u8_count2=0,u8_count3=0,u8_count4=0,u8_return;
+	/*check the expression element by element*/
+	for(u8_i=0;(*(u8arr_expression+u8_i))!='\0';u8_i++)
+	{
+		/*select all parentheses and push them into stack*/
+		if((*(u8arr_expression+u8_i)=='{')||(*(u8arr_expression+u8_i)=='(')||(*(u8arr_expression+u8_i)==')')||(*(u8arr_expression+u8_i)=='}'))
+			{
+				Push(*(u8arr_expression+u8_i));
+			}
+	}
+	/*define the number of iterations we need to count the parentheses*/
+	u8_j=gu8_Top;
+	for(u8_i=0;u8_i<=u8_j;u8_i++)
+	{
+		u8_return=Pull();//pull parentheses element by element
+
+		if(u8_return=='}')
+			{
+				u8_count1++;
+			}
+		else if(u8_return=='{')
+			{
+				u8_count2++;
+			}
+		else if(u8_return==')')
+			{
+				u8_count3++;
+			}
+		else if(u8_return=='(')
+			{
+				u8_count4++;
+			}
+	}
+	/*check if the parentheses balanced or not*/
+	if((u8_count1==u8_count2)&&(u8_count3==u8_count4))
+	{
+		return "Balanced";
+	}
+	else{return "UnBalanced";}
+}
+/*Implementation of push function*/
+void Push(uint8_t u8_data)
+{
+	if(gu8_Top<(size-1))
+	{
+		gu8_Top++;
+		gu8arr_Stackarr[gu8_Top]=u8_data;
+	}
+	else{printf("Stack over flow\n");}
+}
+/*Implementation of pull function*/
+uint8_t Pull(void)
+{
+	uint8_t FuncReturn;
+	if(gu8_Top>-1)
+	{
+		FuncReturn=gu8arr_Stackarr[gu8_Top];
+		gu8_Top--;
+	}
+	else
+	{
+		printf("Stack is empty\n");
+		FuncReturn=0;
+	}
+	return FuncReturn;
+}
